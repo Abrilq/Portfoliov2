00c249de0cdc1a93362d5856d0c51d8fb58d241a
jcl2468 <jclme25@gmail.com>
2025-09-06 03:20:42 +0800

    Update routes.ts

--- my-react-app/app/routes.ts
+++ my-react-app/app/routes.ts
@@ -1,14 +1,14 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes"; 
 
-export default [
-  index("routes/home.tsx"),
-  route("projects/lost-and-found", "routes/projects/lost-and-found.tsx"),
-  route("projects/pre-advising", "routes/projects/pre-advising.tsx"),
-  route("projects/ecommerce", "routes/projects/ecommerce.tsx"),
-  route("projects/ordering-system", "routes/projects/ordering-system.tsx"),
-  route("projects/damath", "routes/projects/damath.tsx"),
-  route("projects/ar-project", "routes/projects/ar-project.tsx"),
-  route("projects/drinking-session", "routes/projects/drinking-session.tsx"),
-  route("projects/srt-renamer", "routes/projects/srt-renamer.tsx"),
+export default [ index("routes/home.tsx"), 
+
+  route("projects/lost-and-found", "routes/projects/lost-and-found.tsx"), 
+  route("projects/pre-advising", "routes/projects/pre-advising.tsx"), 
+  route("projects/ecommerce", "routes/projects/ecommerce.tsx"), 
+  route("projects/ordering-system", "routes/projects/ordering-system.tsx"), 
+  route("projects/damath", "routes/projects/damath.tsx"), 
+  route("projects/ar-project", "routes/projects/ar-project.tsx"), 
+  route("projects/drinking-session", "routes/projects/drinking-session.tsx"), 
+  route("projects/srt-renamer", "routes/projects/srt-renamer.tsx"), 
   route("projects/projectpage", "routes/projects/projectpage.tsx"),
 ] satisfies RouteConfig;
