efee73daff4ecd65717682193968bc49e1db94e8
jcl2468 <jclme25@gmail.com>
2025-09-09 03:08:11 +0800

    retain footer

--- my-react-app/app/routes/projects/lost-and-found.tsx
+++ my-react-app/app/routes/projects/lost-and-found.tsx
@@ -458,11 +458,11 @@ export default function ProjectPage(): ReactElement {
         </ScrollReveal>
 
         {/* Make sure all sections are within this container */}
-        {/* <div className="relative w-full">
+        <div className="relative w-full">
           <footer className="relative w-full text-center py-6 text-gray-500 text-sm mt-auto">
             © {new Date().getFullYear()} John Clarence A. Legaspi
           </footer>
-        </div> */}
+        </div>
 
       </FadeContent>
     </div>
