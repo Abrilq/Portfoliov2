839ccde72d925e014063e839b774db236ea48aaf
jcl2468 <jclme25@gmail.com>
2026-03-24 10:08:12 +0800

    SplashCursor error fixed

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -5,4 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress sourcemap warnings
+        if (warning.code === "SOURCEMAP_ERROR") return;
+        warn(warning);
+      },
+    },
+  },
 });
